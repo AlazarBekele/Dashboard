93e6a703911cacf4d7c6c03e8c0f31c84e9f03d4
AlazarBekele <alazarthe70@gmail.com>
2025-02-06 23:57:48 +0300

    successfully add nav into image

--- EAG/settings.py
+++ EAG/settings.py
@@ -121,6 +121,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'satticfile')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Adjust as needed
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
