8e372b89c739a7b6951d0dab55fc210f160c5490
Alazar Bekele <alazarthe70@gmail.com>
2025-02-16 18:04:41 +0300

    Make background

--- EAG/settings.py
+++ EAG/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+MEDIA_URL='/media/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'satticfile')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
