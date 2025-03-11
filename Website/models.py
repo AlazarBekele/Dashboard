fee346060fd891bee9dab9f19306b95d61d098ae
Apple <alazarthe70@gmail.com>
2025-03-11 18:25:47 +0300

    Change the hero banner image

--- Website/models.py
+++ Website/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Image_Container (models.Model):
 
     image_name = models.CharField (max_length=20)
-    # picture = models.ImageField (upload_to='photo/')
+    picture = models.ImageField (upload_to='photo/')
 
     def __str__(self):
         return self.image_name
@@ -14,7 +14,7 @@ class Image_Container (models.Model):
 class Second_image (models.Model):
 
     Second_img = models.CharField (max_length=20)
-    # Second_picture = models.ImageField (upload_to='photo/')
+    Second_picture = models.ImageField (upload_to='photo/')
 
     def __str__(self):
         return self.Second_img
