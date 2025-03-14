9fb024813943bd9f18b77e6ed165a027224f6b67
Alazar <alazarthe70@gmail.com>
2025-03-14 19:09:46 +0300

    create app.js for fetch data from django url

---
+++ static/Function/app.js
@@ -0,0 +1,13 @@
+
+function FetchData() {
+
+    fetch('api/data') // call api from django url
+
+        .then(response => response.json())
+        .then(data => {
+
+            let display_data = document.getElementById ('')
+
+        })
+
+}
