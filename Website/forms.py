03b317462b0bfecb1f054b61984922fbb699ec47
Alazar Bekele <alazarthe70@gmail.com>
2025-04-05 18:18:43 +0300

    System Update

--- Website/forms.py
+++ Website/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Bible_quote
+from .models import Bible_quote, News_post
 
 class YourModelForm(forms.ModelForm):
     class Meta:
@@ -21,3 +21,39 @@ class YourModelForm(forms.ModelForm):
             })
                 
         }
+
+
+
+class News_upload (forms.ModelForm):
+
+    model = News_post
+    fields = '__all__'
+    lables = { 'title' : '' }
+
+    widgets = {
+
+        'title' : forms.TextInput(attrs={
+            'class' : 'form-control roboto'
+        }),
+
+        'Post_items' : forms.TextInput(attrs={
+            'class' : 'form-control roboto'
+        }),
+
+        'Descrtion' : forms.TextInput(attrs={
+            'class' : 'form-control roboto'
+        }),
+
+        'Reaction_container' : forms.TextInput(attrs={
+            'class' : 'form-control roboto'
+        }),
+
+        'title' : forms.TextInput(attrs={
+            'class' : 'form-control roboto'
+        }),
+
+        'title' : forms.TextInput(attrs={
+            'class' : 'form-control roboto'
+        }),
+
+    }
