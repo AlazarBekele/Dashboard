38f47ff47a946080c15d15e3b28e258a54e24cba
Alazar <alazarthe70@gmail.com>
2025-06-06 23:06:45 +0300

    copy cond form chatgpt

--- Website/youtube_Data_Scrap.py
+++ Website/youtube_Data_Scrap.py
@@ -1,18 +1,27 @@
 from googleapiclient.discovery import build
 from django.shortcuts import render
 
+def youtube_search(request):
+    query = request.GET.get('q', 'django tutorial')
+    api_key = 'AIzaSyAfnebY_AdgczXg8Dmd8We5mZCVaWqYoJo'
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
-def youtube_data (request):
+    request_data = youtube.search().list(
+        q=query,
+        part='snippet',
+        maxResults=5,
+        type='video'
+    )
 
-  query = request.GET.get('q', 'django tutorial')
-  api = 'API KEY'
-  youtube = build ('youtube', 'v3', developerKey=api)
+    response = request_data.execute()
 
-  result = youtube.search().list(
+    videos = []
+    for item in response['items']:
+        video_data = {
+            'title': item['snippet']['title'],
+            'video_id': item['id']['videoId'],
+            'thumbnail': item['snippet']['thumbnails']['high']['url'],
+        }
+        videos.append(video_data)
 
-    q = query,
-    part = 'Alazar',
-    maxResult = 6,
-    type = 'video'
-
-  )
+    return render(request, 'youtube_results.html', {'videos': videos})
