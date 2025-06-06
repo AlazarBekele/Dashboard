5fe1da3f1ca5bd357a2610a4e23422c143e04d0a
Alazar <alazarthe70@gmail.com>
2025-06-06 16:52:28 +0300

    remove context

--- User Profile/User Profile Main/Detail/views.py
+++ User Profile/User Profile Main/Detail/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from .models import Detail_Background_Img
 
 def index (request):
 
-    Background_Data = Detail_Background_Img.objects.first()
-
-    context = {
-
-        'Background_IMG' : Background_Data
-
-    }
-
-    return render (request, 'index.html', context=context)
+    return render (request, 'index.html')
