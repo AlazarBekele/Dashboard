79a80c455597b5ac1597702f5f21e93ba915bc9b
Alazar <alazarthe70@gmail.com>
2025-06-06 16:57:27 +0300

    Create Image Container model

--- User Profile/User Profile Main/Detail/models.py
+++ User Profile/User Profile Main/Detail/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class BG_Img (models.Model):
+  
+  Name = models.CharField (max_length=20)
+  Image = models.ImageField (upload_to='photo/')
+
+  def __str__(self):
+    return self.Name
