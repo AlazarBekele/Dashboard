3169840f8620b3dfa7fa538cb128a5e7d66d564d
Alazar <alazarthe70@gmail.com>
2025-06-06 16:35:55 +0300

    Fix error

--- User Profile/User Profile Main/User_Profile/settings.py
+++ User Profile/User Profile Main/User_Profile/settings.py
@@ -123,9 +123,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'satticfile')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
