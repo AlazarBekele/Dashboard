f5eef1328fe1778a830b6f09cd4084577d649a25
Alazar <alazarthe70@gmail.com>
2025-06-06 17:14:30 +0300

    Add View

--- User Profile/User Profile Main/Detail/views.py
+++ User Profile/User Profile Main/Detail/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import BG_Img
+
 
 def index (request):
 
-    return render (request, 'index.html')
+    Background_Picture = BG_Img.objects().all()
+
+    context = {
+        'Background' : Background_Picture
+    }
+
+    return render (request, 'index.html', context=context)
